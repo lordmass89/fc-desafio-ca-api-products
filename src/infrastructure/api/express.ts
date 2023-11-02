016e1e0d4dc5a5a61aa5760e0e6ede133013dd37
Maicon Souza <maicon.souza@paipe.co>
2023-11-01 22:58:57 -0300

    desafio api products

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,11 +1,14 @@
+import { productRoute } from './routes/product.route';
 import express, { Express } from "express";
 import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
 import { customerRoute } from "./routes/customer.route";
+import ProductModel from "../product/repository/sequelize/product.model";
 
 export const app: Express = express();
 app.use(express.json());
 app.use("/customer", customerRoute);
+app.use("/product", productRoute)
 
 export let sequelize: Sequelize;
 
@@ -15,7 +18,8 @@ async function setupDb() {
     storage: ":memory:",
     logging: false,
   });
-  await sequelize.addModels([CustomerModel]);
+
+  sequelize.addModels([CustomerModel, ProductModel]);
   await sequelize.sync();
 }
 setupDb();
